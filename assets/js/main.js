c6414543b65242c4c5ca8fa686e7d4927d652a72
ShubhamSKA <skumarag@purdue.edu>
2023-10-03 09:47:54 -0400

    made menu closed by default

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,11 +75,11 @@
 
   // Inactive by default on <= large.
   breakpoints.on("<=large", function () {
-    $sidebar.addClass("inactive");
+    //$sidebar.addClass("inactive");
   });
 
  breakpoints.on(">large", function () {
-    $sidebar.removeClass("inactive");
+    //$sidebar.removeClass("inactive");
   });
 
   // Hack: Workaround for Chrome/Android scrollbar position bug.
